edca713a69f88c206553fbfcb6e9c3e7e2506286
Santiago Martin <santiagomartinagra@gmail.com>
2016-02-06 11:39:00 +0100

    Completado tutorial "1 Léxico"

---
+++ practicas/Tutorial/src/sintactico/Tokens.java
@@ -0,0 +1,17 @@
+package sintactico;
+
+public class Tokens {
+	
+	public static final int INT = 257;
+    public static final int REAL = 258;
+    
+	public static final int CODE = 259;
+	public static final int DATA = 260;
+	
+	public static final int PRINT = 261;
+	
+	public static final int LITERALREAL = 262; 
+	static final int LITERALINT = 263;
+	static final int IDENT = 264;
+
+}
