6149329478c4acec89d82997b65b6329f777184e
Santiago Martin <santiagomartinagra@gmail.com>
2016-02-18 12:48:06 +0100

    Comenzado la entrega

---
+++ practicas/Entrega/src/ast/Position.java
@@ -0,0 +1,29 @@
+package ast;
+
+/**
+ * @author Raúl Izquierdo
+ */
+
+public class Position {
+
+	public Position(int line, int column) {
+		this.line = line;
+		this.column = column;
+	}
+	
+	public int getLine() {
+		return line;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+	
+	public String toString() {
+		return line + ":" + column;
+	}
+
+	private int line, column;
+}
+
+
