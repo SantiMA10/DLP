6585284b89d7ca374d49dcddf0f45780b89f8292
Santiago Martin <santiagomartinagra@gmail.com>
2016-03-10 12:24:43 +0100

    [Entrega] Añadido contextMap

---
+++ practicas/Entrega/src/semantico/ContextMap.java
@@ -0,0 +1,48 @@
+package semantico;
+
+import java.util.*;
+/*
+ * Implementación de una tabla Hash con contextos.
+ * Permite:
+ * - Insertar símbolos (put) en el contexto actual.
+ * - Buscar tanto en el contexto actual (getFromTop) como en todos los contextos (getFromAny).
+ * - Crear y destruir contextos mediante las operaciones set y reset.
+ * 
+ * La forma habitual de instanciarla será:
+ * 	ContextMap<String, DefinicionVariable> variables = new ContextMap<String, DefinicionVariable>();
+ * 
+ */
+public class ContextMap<S, D> {
+
+	public ContextMap() {
+		set();
+	}
+
+	public void put(S nombre, D def) {
+		contextos.peek().put(nombre, def);
+	}
+
+	public D getFromTop(S nombre) {
+		return contextos.peek().get(nombre);
+	}
+
+	public D getFromAny(S nombre) {
+		for (int i = contextos.size() - 1; i >= 0; i--) {
+			Map<S, D> contexto = contextos.get(i);
+			D def = contexto.get(nombre);
+			if (def != null)
+				return def;
+		}
+		return null;
+	}
+
+	public void set() {
+		contextos.push(new HashMap<S, D>());
+	}
+
+	public void reset() {
+		contextos.pop();
+	}
+
+	private Stack<Map<S, D>> contextos = new Stack<Map<S, D>>();
+}
